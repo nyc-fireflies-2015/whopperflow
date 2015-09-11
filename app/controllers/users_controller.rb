0fb85295503f1b55f8ee8bb8a03a8e9ffd70b78f
Kelly Ripple <email": null>
2015-09-11 17:32:22 -0400

    working on user authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,45 +8,27 @@ class UsersController < ApplicationController
   end
 
   def create # POST
-<<<<<<< c33b997146b818c89b94c490461a85e3a512db84
-      @user = User.new(user_params)
+    @user = User.new(user_params)
     if @user.save
       redirect_to user_url(@user)
     else
       flash[:error] = @user.errors.full_messages
       redirect_to new_user_path
-=======
-    @user = User.build(user_params)
-    if @user.save
-      redirect_to profile(@user)
-    else
-      flash[:error] = @user.errors.full_messages
->>>>>>> CRUD user
     end
   end
 
   def login # POST
     @user = User.find_by(username: params[:username]).try(:authenticate, params[:password])
-<<<<<<< c33b997146b818c89b94c490461a85e3a512db84
-    p @user
     if @user
       session[:user_id] = @user.id
       redirect_to user_url(@user)
     else
       flash[:error] = 'incorrect username or password'
       redirect_to users_path
-=======
-    if @user
-      session[:user_id] = @user.id
-      redirect_to profile(@user)
-    else
-      flash[:error] = @user.errors.full_messages
->>>>>>> CRUD user
     end
   end
 
   def show # profile
-<<<<<<< c33b997146b818c89b94c490461a85e3a512db84
     @user = User.find_by(id: session[:user_id]) #check a better way to do this
   end
 
@@ -57,10 +39,6 @@ class UsersController < ApplicationController
   #   end
   # end
 
-=======
-  end
-
->>>>>>> CRUD user
   private
 
   def user_params
