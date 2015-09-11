376d51e35238bc8703407ee3a6ae82b43971c30b
ZedforZardoz <loweja01@gmail.com>
2015-09-11 19:38:07 -0400

    rebase master

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+# describe UserController do
+#   describe 'user access' do
+#     before :each do
+#       @user = FactoryGirl.create(:user)
+#       session[:user_id] = @user.id
+#     end
+
+  
+
+# end
