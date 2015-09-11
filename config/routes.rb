f1563ce2db5f043b9d9f07c3e4e4aa30f64e7178
Kelly Ripple <email": null>
2015-09-11 16:22:35 -0400

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+   get '/login' => 'users#login'
+   get '/register' => 'users#register'
+   get '/profile' => 'users#show'
+
+   resources :questions
+   resources :comments
+   resources :answers
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
