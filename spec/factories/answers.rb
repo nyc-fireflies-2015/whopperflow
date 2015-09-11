8e7a191c34fa72a96585f11d10957363d67fc89d
ZedforZardoz <loweja01@gmail.com>
2015-09-11 14:47:49 -0400

    fix indentations

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
-	factory :answer do
-		content { Faker::Lorem.paragraph }
-	end
-	association :question, factory: :question
-	association :authory, factory: :user
+  factory :answer do
+    content { Faker::Lorem.paragraph }
+  end
+  association :question, factory: :question
+  association :author, factory: :user
 end
 
