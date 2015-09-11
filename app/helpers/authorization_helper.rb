a7e8322714f5f93139ae652eabcabd49fa4f6f40
Kelly Ripple <email": null>
2015-09-11 18:35:12 -0400

    add user helper methods

---
+++ app/helpers/authorization_helper.rb
@@ -0,0 +1,12 @@
+def logged_in?
+  session[:user_id]
+end
+
+def current_user
+  User.find_by(id: session[:user_id])
+end
+
+def redirect_to_login_unless_logged_in
+  flash[:error] = "You must be logged in to do that!" unless logged_in?
+  redirect '/users/login' unless logged_in?
+end
