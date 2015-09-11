d0ce5ced298b33ca3bf32ecf932125c3a22a12ee
James Lowenthal <email": null>
2015-09-11 17:32:04 -0400

    Add tests for answers controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150911185514) do
-  Create models and migrations
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,4 +60,5 @@ ActiveRecord::Schema.define(version: 20150911185514) do
   end
 
   add_index "votes", ["votable_type", "votable_id"], name: "index_votes_on_votable_type_and_votable_id", using: :btree
+
 end
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+describe UserController do
+
+
+end
