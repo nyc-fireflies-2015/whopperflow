2fd36a3767f00bf81bb2603411812cf9dcc2e3d0
ZedforZardoz <loweja01@gmail.com>
2015-09-11 15:23:44 -0400

    Final indentation fix

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
 describe Question do
-	context 'associations' do
-		it 'should have many answers' do
-			should have_many :answers
-		end
-		it 'should have one best_answer (class name Answer)' do
-			should have_one(:best_answer).class_name('Answer')
-		end
-		it 'should have many comments' do
-			should have_many :comments
-		end
-		it 'should belong to author (class name User)' do
-			should belong_to(:author).class_name('User')
-		end
-		it 'should have many votes' do
-			should have_many :votes
-		end
-	end
+  context 'associations' do
+    it 'should have many answers' do
+      should have_many :answers
+    end
+    it 'should have one best_answer (class name Answer)' do
+      should have_one(:best_answer).class_name('Answer')
+    end
+    it 'should belong to comments' do
+      should have_many :comments
+    end
+    it 'should belong to author (class name User)' do
+      should belong_to(:author).class_name('User')
+    end
+    it 'should have many votes'do
+      should have_many :votes
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
 describe User do
-	context 'associations' do
-		it 'should have many comments' do
-			should have_many :comments
-		end
-		it 'should have many answers' do
-			should have_many :answers
-		end
-		it 'should have many questions' do
-			should have_many :questions
-		end
-	end
+  context 'associations' do
+    it 'should have many comments' do
+      should have_many :comments
+    end
+    it 'should have many answers' do
+      should have_many :answers
+    end
+    it 'should have many questions' do
+      should have_many :questions
+    end
+  end
 end
