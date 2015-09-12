cbc4dc82b4d063c6ea698582b64c1290f66a72b7
Alexander <ahuang62wp7c@gmail.com>
2015-09-12 19:59:10 -0400

    test get index questions spec

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -4,6 +4,15 @@ include LoginMacros
 describe QuestionsController, type: :controller do
 	let!(:user) { FactoryGirl.create(:user) }
 
+  describe "GET #index" do
+    context 'questions page' do
+      it 'assigns all questions to @questions' do
+        get :index
+        expect(assigns(:questions)).to eq Question.all
+      end
+    end
+  end
+
 	describe 'GET #new' do
 		context 'is logged in' do
       it "assigns a new question to @question" do
@@ -15,32 +24,38 @@ describe QuestionsController, type: :controller do
 	end
 
 	describe 'POST #create' do
-    context 'valid answer' do
+    context 'valid question params' do
       before :each do
         login(user)
         post :create, question: FactoryGirl.attributes_for(:question)
       end
+
       it "creates a new question in the database" do
       	expect(Question.count).to eq 1
       end
+
       it "redirects to Question#show" do
-        # binding.pry
         expect(response).to redirect_to Question.last
       end
     end
-    # context 'invalid answer' do
-    #   it "creates a new answer in the database" do
-    #     expect{
-    #       post :create, id: question.id,
-    #         answer: {content: nil}
-    #     }.to change(questions.answers.count).by 0
-    #   end
-    #   it "redirects to answers#new" do
+
+    context 'invalid question params' do
+      before :each do
+        login(user)
+        # binding.pry
+        # post :create, question: FactoryGirl.attributes_for(:invalid_question)
+        post :create, question:{title: "Hello World", content: nil}
+      end
+
+      it "does not create a question in the database" do
+        expect(response).to render_template :new
+      end
+      # it "redirects to answers#new" do
     #     post :create, id: question.id,
     #     answer: {content: nil}
     #     expect(response).to redirect_to new_answer
     #   end
-    # end
+    end
   end
 
 end
