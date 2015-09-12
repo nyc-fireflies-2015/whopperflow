16d52dd5e503fdc0bfb2e4afd4e6e754c563ac86
Alexander <ahuang62wp7c@gmail.com>
2015-09-12 18:53:52 -0400

    create question spec

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
+include LoginMacros
 
-# RSpec.describe QuestionsController, type: :controller do
+describe QuestionsController, type: :controller do
+	let!(:user) { FactoryGirl.create(:user) }
 
-# end
+	describe 'GET #new' do
+		context 'is logged in' do
+      it "assigns a new question to @question" do
+        login(user)
+        get :new
+        expect(assigns(:question)).to be_a_new(Question)
+      end
+    end
+	end
+
+	describe 'POST #create' do
+    context 'valid answer' do
+      it "creates a new question in the database" do
+      	login(user)
+      	post :create, question: FactoryGirl.attributes_for(:question)
+      	expect(Question.count).to eq 1
+      end
+      # it "redirects to Question#show" do
+      #   post :create, question: FactoryGirl.attributes_for(:question)
+      #   expect(response).to redirect_to question_path(question)
+      # end
+    end
+    # context 'invalid answer' do
+    #   it "creates a new answer in the database" do
+    #     expect{
+    #       post :create, id: question.id,
+    #         answer: {content: nil}
+    #     }.to change(questions.answers.count).by 0
+    #   end
+    #   it "redirects to answers#new" do
+    #     post :create, id: question.id,
+    #     answer: {content: nil}
+    #     expect(response).to redirect_to new_answer
+    #   end
+    # end
+  end
+
+end
