75e41db757f580fd6bcef832bdd505f48b45cf28
Kelly Ripple <email": null>
2015-09-12 17:16:59 -0400

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   post '/users/login' => 'users#login'
   delete '/users/logout' => 'users#logout'
 
+  root 'questions#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
