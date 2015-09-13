3bad059c0f0e0a3099b4c3bdc2c232c7d0705ad7
Alexander <ahuang62wp7c@gmail.com>
2015-09-12 20:07:34 -0400

    add index render template spec for question

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -4,14 +4,26 @@ include LoginMacros
 describe QuestionsController, type: :controller do
 	let!(:user) { FactoryGirl.create(:user) }
 
-  describe "GET #index" do
+  describe 'GET #index' do
     context 'questions page' do
       it 'assigns all questions to @questions' do
         get :index
         expect(assigns(:questions)).to eq Question.all
       end
+
+      it "renders the :index template" do
+        # binding.pry
+        get :index
+        expect(response).to render_template :index
+      end
     end
   end
+
+  # describe 'GET #show' do
+  #   context 'view specific question' do
+  #     it ''
+  #   end
+  # end
 
 	describe 'GET #new' do
 		context 'is logged in' do
@@ -39,23 +51,23 @@ describe QuestionsController, type: :controller do
       end
     end
 
-    context 'invalid question params' do
-      before :each do
-        login(user)
+    # context 'invalid question params' do
+    #   before :each do
+    #     login(user)
         # binding.pry
         # post :create, question: FactoryGirl.attributes_for(:invalid_question)
-        post :create, question:{title: "Hello World", content: nil}
-      end
+        # post :create, question:{title: "Hello World", content: nil}
+      # end
 
-      it "does not create a question in the database" do
-        expect(response).to render_template :new
-      end
+      # it "does not create a question in the database" do
+      #   expect(response).to render_template :new
+      # end
       # it "redirects to answers#new" do
     #     post :create, id: question.id,
     #     answer: {content: nil}
     #     expect(response).to redirect_to new_answer
     #   end
-    end
+    # end
   end
 
 end
