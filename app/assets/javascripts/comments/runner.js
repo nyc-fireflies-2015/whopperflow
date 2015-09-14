1d8a2d5322cb59277a911416857a6428b9d73d46
ZedforZardoz <loweja01@gmail.com>
2015-09-13 23:27:04 -0400

    Add comment runner

---
+++ app/assets/javascripts/comments/runner.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  var ctrl = new CommentController();
+  $(document).on("click", ".add_comment" function(event){
+    event.preventDefault();
+    ctrl.getNewCommentForm(event.target);
+  });
+});
