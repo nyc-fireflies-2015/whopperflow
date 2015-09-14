0e35b3b2f162689cef9c6181491eef295d12e4c0
ZedforZardoz <loweja01@gmail.com>
2015-09-13 22:30:16 -0400

    Add self referencing relationship to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :commentable, polymorphic: true
   belongs_to :author, class_name: "User"
+  has_many :comments, as: :commentable
   validates_presence_of :content
   def associated_question
     case self.commentable_type
